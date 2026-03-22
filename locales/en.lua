e3b6f65ecfeeb315bc68658413a239c73ec23d67
Helix Dev <dev@helix-scripts.dev>
2026-03-22 02:34:53 +0100

    feat: add English locale file with default translations

---
+++ locales/en.lua
@@ -0,0 +1,28 @@
+--- English locale for helix_lib
+return {
+    -- General
+    error_unknown = 'An unknown error occurred.',
+    error_no_permission = 'You do not have permission to do this.',
+    error_invalid_input = 'Invalid input provided.',
+
+    -- Framework bridge
+    player_not_found = 'Player not found.',
+    money_added = '$%s has been added to your account.',
+    money_removed = '$%s has been removed from your account.',
+    insufficient_funds = 'You do not have enough money.',
+
+    -- Notifications
+    notify_success = 'Success',
+    notify_error = 'Error',
+    notify_warning = 'Warning',
+    notify_info = 'Info',
+
+    -- Version check
+    version_up_to_date = 'You are running the latest version.',
+    version_update_available = 'An update is available: v%s',
+
+    -- Config
+    config_loaded = 'Configuration loaded.',
+    config_reloaded = 'Configuration reloaded.',
+    config_error = 'Failed to load configuration.',
+}
